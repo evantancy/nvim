40b28ad33746571f8c2ad5e425536e96f90f0421
Evan Tan <tcyevan@gmail.com>
2022-04-11 11:25:23 +0800

    refactor: add settings for vscode neovim, format using stylua

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -1,97 +1,112 @@
 -- Leader
-vim.g.mapleader = " "
+vim.g.mapleader = ' '
 
 local opts = { noremap = true }
 local expr_opts = { noremap = true, expr = true }
 
-if vim.fn.exists("g:vscode") == 1 then
-	-- inside vscode
+if vim.fn.exists('g:vscode') == 1 then
+    -- inside vscode
 
-	-- navigate buffer
-	map("n", "<tab>", '<cmd>call VSCodeNotify("workbench.action.nextEditor")<cr>')
-	map("n", "<s-tab>", '<cmd>call VSCodeNotify("workbench.action.previousEditor")<cr>')
-	-- splits
-	map("n", "<C-h>", '<cmd>call VSCodeNotify("workbench.action.navigateLeft")<cr>')
-	map("n", "<C-j>", '<cmd>call VSCodeNotify("workbench.action.navigateDown")<cr>')
-	map("n", "<C-k>", '<cmd>call VSCodeNotify("workbench.action.navigateUp")<cr>')
-	map("n", "<C-l>", '<cmd>call VSCodeNotify("workbench.action.navigateRight")<cr>')
-	-- nvim-tree
-	map("n", "<c-n>", "<cmd>call VSCodeNotify('workbench.action.toggleSidebarVisibility')<cr>", opts)
+    -- allow single line travel when lines visually wrap
+    -- not working atm, unless you manually set in vscode neovim
+    map('n', 'k', 'gk', { noremap = false })
+    map('n', 'j', 'gj', { noremap = false })
+    -- navigate buffer
+    map('n', '<tab>', '<cmd>call VSCodeNotify("workbench.action.nextEditor")<cr>')
+    map('n', '<s-tab>', '<cmd>call VSCodeNotify("workbench.action.previousEditor")<cr>')
+    -- splits
+    map('n', '<C-h>', '<cmd>call VSCodeNotify("workbench.action.navigateLeft")<cr>')
+    map('n', '<C-j>', '<cmd>call VSCodeNotify("workbench.action.navigateDown")<cr>')
+    map('n', '<C-k>', '<cmd>call VSCodeNotify("workbench.action.navigateUp")<cr>')
+    map('n', '<C-l>', '<cmd>call VSCodeNotify("workbench.action.navigateRight")<cr>')
+    -- nvim-tree
+    map('n', '<c-n>', "<cmd>call VSCodeNotify('workbench.action.toggleSidebarVisibility')<cr>", opts)
 
-	-- see `lsp.lua`
-	map("n", "K", '<cmd>call VSCodeNotify("editor.action.showHover")<cr>')
-	map("n", "gd", '<cmd>call VSCodeNotify("editor.action.revealDefinition")<cr>')
-	map("n", "gr", '<cmd>call VSCodeNotify("editor.action.goToReferences")<cr>')
+    -- see `lsp.lua`
+    map('n', 'K', '<cmd>call VSCodeNotify("editor.action.showHover")<cr>')
+    map('n', 'gd', '<cmd>call VSCodeNotify("editor.action.revealDefinition")<cr>')
+    map('n', 'gr', '<cmd>call VSCodeNotify("editor.action.goToReferences")<cr>')
 
-	-- Comment.nvim
-	-- -- map("x",)
-	-- xmap gc  <Plug>VSCodeCommentary
-	-- nmap gc  <Plug>VSCodeCommentary
-	-- omap gc  <Plug>VSCodeCommentary
-	-- nmap gcc <Plug>VSCodeCommentaryLine
-	--
-	-- xmap <C-/> <Plug>VSCodeCommentarygv
-	-- nmap <C-/> <Plug>VSCodeCommentaryLine
+    -- Comment.nvim
+    -- xmap gc  <Plug>VSCodeCommentary
+    -- nmap gc  <Plug>VSCodeCommentary
+    -- omap gc  <Plug>VSCodeCommentary
+    -- nmap gcc <Plug>VSCodeCommentaryLine
 
-	-- move hightlighted text up/down
-	map("n", "<space>k", '<cmd>call VSCodeCallVisual("editor.action.moveLinesUpAction, 1)<cr>')
-	map("n", "<space>j", '<cmd>call VSCodeCallVisual("editor.action.moveLinesDownAction, 1)<cr>')
+    -- xmap <C-/> <Plug>VSCodeCommentarygv
+    -- nmap <C-/> <Plug>VSCodeCommentaryLine
+
+    -- move hightlighted text up/down
+    map('n', '<space>k', '<cmd>call VSCodeCallVisual("editor.action.moveLinesUpAction, 1)<cr>')
+    map('n', '<space>j', '<cmd>call VSCodeCallVisual("editor.action.moveLinesDownAction, 1)<cr>')
 else
-	-- inside vim
+    -- inside vim
 
-	-- navigate buffer
-	map("n", "<tab>", "<cmd>bnext<cr>", opts)
-	map("n", "<s-tab>", "<cmd>bprevious<cr>", opts)
-	-- splits
-	map("n", "<c-h>", "<c-w>h")
-	map("n", "<c-j>", "<c-w>j")
-	map("n", "<c-k>", "<c-w>k")
-	map("n", "<c-l>", "<c-w>l")
-	-- nvim-tree
-	map("n", "<c-n>", "<cmd>lua require('nvim-tree').toggle(false, false)<cr>", opts)
+    -- allow single line travel when lines visually wrap
+    map('n', 'k', "v:count == 0 ? 'gk' : 'k'", expr_opts)
+    map('n', 'j', "v:count == 0 ? 'gj' : 'j'", expr_opts)
 
-	-- Comment.nvim
-	-- ctrl+/ or ctrl+\ to line/block comment
-	map("n", "<c-_>", "<cmd> lua require('Comment.api').toggle_current_linewise()<cr>")
-	map("n", "<c-bslash>", "<cmd> lua require('Comment.api').toggle_current_blockwise()<cr>")
-	-- VISUAL MODE COMMENTS
-	map("x", "<c-_>", '<ESC><CMD>lua require("Comment.api").toggle_linewise_op(vim.fn.visualmode())<CR>')
-	map("x", "<c-bslash>", '<ESC><CMD>lua require("Comment.api").toggle_blockwise_op(vim.fn.visualmode())<CR>')
+    -- navigate buffer
+    map('n', '<tab>', '<cmd>bnext<cr>', opts)
+    map('n', '<s-tab>', '<cmd>bprevious<cr>', opts)
+    -- splits
+    map('n', '<c-h>', '<c-w>h')
+    map('n', '<c-j>', '<c-w>j')
+    map('n', '<c-k>', '<c-w>k')
+    map('n', '<c-l>', '<c-w>l')
+    -- nvim-tree
+    map('n', '<c-n>', "<cmd>lua require('nvim-tree').toggle(false, false)<cr>", opts)
 
-	-- move hightlighted text up/down
-	map("n", "<space>j", ":m .+1<CR>==", opts)
-	map("n", "<space>k", ":m .-2<CR>==", opts)
-	map("i", "<c-j>", "<esc>:m .+1<CR>==gi", opts)
-	map("i", "<c-k>", "<esc>:m .-2<CR>==gi", opts)
-	map("v", "J", ":m '>+1<CR>gv=gv", opts)
-	map("v", "K", ":m '<-2<CR>gv=gv", opts)
+    -- Comment.nvim
+    -- ctrl+/ or ctrl+\ to line/block comment
+    map('n', '<c-_>', "<cmd> lua require('Comment.api').toggle_current_linewise()<cr>")
+    map('n', '<c-bslash>', "<cmd> lua require('Comment.api').toggle_current_blockwise()<cr>")
+    -- VISUAL MODE COMMENTS
+    map('x', '<c-_>', '<ESC><CMD>lua require("Comment.api").toggle_linewise_op(vim.fn.visualmode())<CR>')
+    map('x', '<c-bslash>', '<ESC><CMD>lua require("Comment.api").toggle_blockwise_op(vim.fn.visualmode())<CR>')
+
+    -- move hightlighted text up/down
+    map('n', '<space>j', ':m .+1<CR>==', opts)
+    map('n', '<space>k', ':m .-2<CR>==', opts)
+    map('i', '<c-j>', '<esc>:m .+1<CR>==gi', opts)
+    map('i', '<c-k>', '<esc>:m .-2<CR>==gi', opts)
+    map('v', 'J', ":m '>+1<CR>gv=gv", opts)
+    map('v', 'K', ":m '<-2<CR>gv=gv", opts)
+
+    -- Telescope | ff -> find file | fg -> find grep | fb -> find buffer
+    -- Telescope | dl -> diagnostics list | fa -> find all
+    map('n', '<space>vrc', "<cmd>lua require('core.utils').search_dotfiles()<cr>")
+    map('n', '<space>vrg', "<cmd>lua require('core.utils').grep_dotfiles()<cr>")
+    map('n', '<space>ff', "<cmd>lua require('telescope.builtin').find_files({hidden=true})<cr>", opts)
+    map('n', '<space>fg', "<cmd>lua require('telescope.builtin').live_grep()<cr>", opts)
+    map('n', '<space>fb', "<cmd>lua require('telescope.builtin').buffers()<cr>", opts)
+    map('n', '<space>fh', "<cmd>lua require('telescope.builtin').help_tags()<cr>", opts)
+    map('n', '<space>fd', "<cmd>lua require('telescope.builtin').diagnostics()<cr>", opts)
+    map('n', '<space>fa', "<cmd>lua require('telescope.builtin').lsp_references()<cr>", opts)
 end
 
 -- toggle ignorecase
-map("n", "<F2>", "<cmd>set ignorecase! ignorecase?<cr>")
-map("n", "<c-c>", "<esc>", opts)
+map('n', '<F2>', '<cmd>set ignorecase! ignorecase?<cr>')
+map('n', '<c-c>', '<esc>', opts)
 -- exit insert mode whenever you type 'jk' or 'kj'
-map("i", "kj", "<esc>", opts)
-map("i", "jk", "<esc>", opts)
--- allow single line travel when lines visually wrap
-map("n", "k", "v:count == 0 ? 'gk' : 'k'", expr_opts)
-map("n", "j", "v:count == 0 ? 'gj' : 'j'", expr_opts)
+map('i', 'kj', '<esc>', opts)
+map('i', 'jk', '<esc>', opts)
 -- delete without yanking
-map("n", "<space>d", '"_d', opts)
-map("v", "<space>d", '"_d', opts)
+map('n', '<space>d', '"_d', opts)
+map('v', '<space>d', '"_d', opts)
 --  replace currently selected text with default register without yanking
-map("v", "p", '"_dP', opts)
-map("n", "<space>x", '"_dx', opts)
+map('v', 'p', '"_dP', opts)
+map('n', '<space>x', '"_dx', opts)
 
 -- wtf?????????????????????????????????????????????????????????????????????
-map("n", "Y", "y$", opts)
-map("n", "n", "nzzzv", opts)
-map("n", "N", "Nzzzv", opts)
-map("n", "<space>J", "mzJ`z", opts)
-map("i", ",", ",<c-g>u", opts)
-map("i", ".", ".<c-g>u", opts)
-map("i", "!", "!<c-g>u", opts)
-map("i", "?", "?<c-g>u", opts)
+map('n', 'Y', 'y$', opts)
+map('n', 'n', 'nzzzv', opts)
+map('n', 'N', 'Nzzzv', opts)
+map('n', '<space>J', 'mzJ`z', opts)
+map('i', ',', ',<c-g>u', opts)
+map('i', '.', '.<c-g>u', opts)
+map('i', '!', '!<c-g>u', opts)
+map('i', '?', '?<c-g>u', opts)
 -- vim.cmd([[
 -- nnoremap <expr> k (v:count > 5 ? "m'" . v:count : "") . 'k'
 -- nnoremap <expr> j (v:count > 5 ? "m'" . v:count : "") . 'j'
@@ -99,33 +114,22 @@ map("i", "?", "?<c-g>u", opts)
 
 -- make vim behave
 -- D copies highlighted text
-map("v", "D", "y'>p", opts)
+map('v', 'D', "y'>p", opts)
 -- tab while code selected
-map("v", "<", "<gv", opts)
-map("v", ">", ">gv", opts)
+map('v', '<', '<gv', opts)
+map('v', '>', '>gv', opts)
 -- map("n", "<space>b", "<cmd>b <c-d>", opts)
 -- map("n", "<space>b", "<cmd>ls<cr>:b<space>", opts)
 
--- Telescope | ff -> find file | fg -> find grep | fb -> find buffer
--- Telescope | dl -> diagnostics list | fa -> find all
-map("n", "<space>vrc", "<cmd>lua require('core.utils').search_dotfiles()<cr>")
-map("n", "<space>vrg", "<cmd>lua require('core.utils').grep_dotfiles()<cr>")
-map("n", "<space>ff", "<cmd>lua require('telescope.builtin').find_files({hidden=true})<cr>", opts)
-map("n", "<space>fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>", opts)
-map("n", "<space>fb", "<cmd>lua require('telescope.builtin').buffers()<cr>", opts)
-map("n", "<space>fh", "<cmd>lua require('telescope.builtin').help_tags()<cr>", opts)
-map("n", "<space>fd", "<cmd>lua require('telescope.builtin').diagnostics()<cr>", opts)
-map("n", "<space>fa", "<cmd>lua require('telescope.builtin').lsp_references()<cr>", opts)
-
 -- Harpoon
-map("n", "<leader>a", "<cmd>lua require('harpoon.mark').add_file()<cr>")
-map("n", "<leader>h", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>")
-map("n", "<leader>tc", "<cmd>lua require('harpoon.cmd-ui').toggle_quick_menu()<cr>")
-map("n", "<A-1>", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>")
-map("n", "<A-2>", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>")
-map("n", "<A-3>", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>")
-map("n", "<A-4>", "<cmd>lua require('harpoon.ui').nav_file(4)<cr>")
+map('n', '<leader>a', "<cmd>lua require('harpoon.mark').add_file()<cr>")
+map('n', '<leader>h', "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>")
+map('n', '<leader>tc', "<cmd>lua require('harpoon.cmd-ui').toggle_quick_menu()<cr>")
+map('n', '<A-1>', "<cmd>lua require('harpoon.ui').nav_file(1)<cr>")
+map('n', '<A-2>', "<cmd>lua require('harpoon.ui').nav_file(2)<cr>")
+map('n', '<A-3>', "<cmd>lua require('harpoon.ui').nav_file(3)<cr>")
+map('n', '<A-4>', "<cmd>lua require('harpoon.ui').nav_file(4)<cr>")
 -- undotree
-map("n", "<F5>", "<cmd>UndotreeToggle<cr>")
+map('n', '<F5>', '<cmd>UndotreeToggle<cr>')
 -- -- ripgrep
 -- map("n", "<space>ps", " :Rg<space>", opts)
