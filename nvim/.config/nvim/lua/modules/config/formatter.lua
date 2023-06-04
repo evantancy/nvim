683f652d06df9fc5a309c6167add447c612f28f7
Evan Tan <36868915+evantancy@users.noreply.github.com>
2023-06-04 17:39:04 +0800

    feat(vim): ensure nvim works with vscode-neovim

--- nvim/.config/nvim/lua/modules/config/formatter.lua
+++ nvim/.config/nvim/lua/modules/config/formatter.lua
@@ -7,9 +7,34 @@ local format = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 local ca = null_ls.builtins.code_actions
 -- Disable inline diagnostics
-vim.diagnostic.config({
-    virtual_text = true,
-})
+if not vim.g.vscode then
+    vim.diagnostic.config(
+        {
+            virtual_text = {
+                severity_sort = true,
+                severity = {
+                    min = vim.diagnostic.severity.WARN,
+                },
+                source = 'always',
+            },
+        }
+        -- opts = {
+        --     -- options for vim.diagnostic.config()
+        --     diagnostics = {
+        --         underline = true,
+        --         update_in_insert = false,
+        --         virtual_text = {
+        --             spacing = 4,
+        --             source = "if_many",
+        --             prefix = "●",
+        --             -- this will set set the prefix to a function that returns the diagnostics icon based on the severity
+        --             -- this only works on a recent 0.10.0 build. Will be set to "●" when not supported
+        --             -- prefix = "icons",
+        --         },
+        --         severity_sort = true,
+        -- }}
+    )
+end
 
 null_ls.setup({
     sources = {
@@ -37,6 +62,7 @@ null_ls.setup({
         format.gofmt,
         format.shfmt.with({
             extra_args = { '-i', '2', '-sr', '-s', '-ci' },
+            filetypes = { 'bash', 'csh', 'ksh', 'sh', 'zsh' },
         }),
         format.clang_format,
         format.cmake_format,
